47903172cfc8f566c9d00aec0395f2d10ccd7446
Raiyan109 <kraiyan109@gmail.com>
2025-01-11 11:25:13 +0600

    ok

--- client/src/components/Signup.jsx
+++ client/src/components/Signup.jsx
@@ -1,11 +1,30 @@
 import { useState } from "react"
+import axios from "axios"
+import Cookies from 'universal-cookie'
 
 
 const Signup = () => {
     const [user, setUser] = useState(null)
+    const cookies = new Cookies();
 
     const signup = async () => {
+        console.log(user);
+        try {
+            await axios.post('http://localhost:3000/signup', user).then((res) => {
+                const { token, firstName, lastName, userName, hashedPassword } = res.data;
+                cookies.set('token', token);
+                cookies.set('userId', token);
+                cookies.set('username', userName);
+                cookies.set('firstName', firstName);
+                cookies.set('lastname', lastName);
+                cookies.set('hashedpassword', hashedPassword);
 
+            })
+
+
+        } catch (error) {
+            console.log(error)
+        }
     }
     return (
         <div className="auth-container">
--- server/src/index.js
+++ server/src/index.js
@@ -1,6 +1,9 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors';
+import { StreamChat } from 'stream-chat';
+import { v4 as uuidv4 } from 'uuid';
+import bcrypt from 'bcrypt';
 
 const app = express();
 const port = 3000;
@@ -10,6 +13,37 @@ app.use(express.json());
 
 const apiKey = 'tng54gvqq64m'
 const apiSecret = 'rzztu2h9cxqfv5wat2yujjpebddqqe6etb7t3b6cjgvxju4mqz6dxsmede4ppqu8'
+const serverClient = new StreamChat.getInstance(apiKey, apiSecret);
+
+app.post('/signup', async (req, res) => {
+    try {
+        const { firstName, lastName, userName, password } = req.body;
+
+        if (!password) {
+            return res.status(400).json({ error: 'Password is required' });
+        }
+
+        const userId = uuidv4();
+        const hashedPassword = await bcrypt.hash(password, 10);
+        const token = serverClient.createToken(userId);
+
+
+        res.json({
+            token,
+            firstName,
+            lastName,
+            userName,
+            hashedPassword,
+        });
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ error: 'Something went wrong' });
+    }
+});
+
+app.post('/login', (req, res) => {
+    res.send('Hello World!');
+});
 
 app.get('/', (req, res) => {
     res.send('Hello World!');
